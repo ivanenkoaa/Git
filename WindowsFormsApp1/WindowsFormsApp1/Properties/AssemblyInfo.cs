dc2119150d5c4f502c9401ee30216896cb3f1fb3
Артем Иваненко <artemivan1998@mail.ru>
2018-11-11 19:44:00 +0400

    5

--- WindowsFormsApp1/WindowsFormsApp1/Properties/AssemblyInfo.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("WindowsFormsApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("WindowsFormsApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("2ac8f024-5a22-4a71-a082-81c5107116ed")]
+[assembly: Guid("813fab99-0b59-4a7e-9069-e904987eae04")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
